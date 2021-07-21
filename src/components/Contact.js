4b82ddce26828b3ca41bd685277eb5765d1fda02
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-07-21 10:24:38 -0700

    added contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,17 @@
 import React from "react"
 
+import { SocialIcon } from "react-social-icons";
+
 export default function Contact() {
-    return <h1>Contact</h1>
+    return(
+        <main>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h2 className="text-4xl text-blue leading-none lg:leading-snug ">Where to contact me </h2>
+                <div>
+                    <SocialIcon url="https://github.com/MarissaNancy" className="mr-4" target="_blank" style={{ height: 30, width: 30}} />
+                    <SocialIcon url="https://www.linkedin.com/in/nancy-m-gonzalez/" className="mr-4" target="_blank" style={{ height: 30, width: 30}}/>
+                    </div>
+            </section>
+        </main>
+    )
 }
