a45b7c18fc43298557a2ec18e4538aa5ca808562
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-07-21 19:27:26 -0700

    added pics

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from "react"
-import image from "../pic.jpg"
+import image from "../Assets/bg1.png"
 
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="Beach" className="absolute object-cover w-full h-full"/>
+            <img src={image} alt="textured background with leaves" className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-white leading-none lg:leading-snug ">Welcome to my portfolio!</h1>
-            </section>
+                <h1 className="text-5xl text-blue-600 leading-none lg:leading-snug ">Welcome to Nancy's portfolio!</h1>
+                <div classname="justify-center text-blue">
+                    <p>ahhhh</p>
+                </div>
+                </section>
         </main>
     )
 }
